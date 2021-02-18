87ee1062ce8d1b4f96824e4b9ee43e7cfa239cf5
Kendy Nguyen <kendyisnguyen@gmail.com>
2021-02-18 15:12:19 -0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,40 @@ module.exports = {
   extends: [
     "plugin:@typescript-eslint/recommended",
     "plugin:prettier/recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
     "prettier/@typescript-eslint",
+    "eslint:recommended",
+    "prettier",
+    "prettier/react",
   ],
-  plugins: ["@typescript-eslint", "prettier"],
+  plugins: [
+    "@emotion",
+    "@typescript-eslint",
+    "import",
+    "jsx-a11y",
+    "prettier",
+    "react-hooks",
+  ],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
+    es6: true,
     browser: true,
     node: true,
   },
   rules: {
+    // "react/prop-types": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "no-console": "warn",
+    "@emotion/pkg-renaming": "error",
+    "@emotion/jsx-import": "error",
     quotes: "off",
     "@typescript-eslint/quotes": [
       2,
@@ -33,5 +55,10 @@ module.exports = {
         printWidth: 120,
       },
     ],
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
   },
 };
